c3bfcc8f32f3e6b082c434b3e7d0e07466d4aa29
MrEkwunife <ekwunifechimobi16@gmail.com>
2026-01-28 09:33:03 +0100

    ref: rea-arranged codebase

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { readConfig } from "./src/config";
 
 export default defineConfig({
   schema: "src/lib/db",
-  out: "src/lib/db",
+  out: "src/lib/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: readConfig().dbUrl,
